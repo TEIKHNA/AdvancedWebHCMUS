df7887b4f5875b16f6b2eb6e0894f22da356decf
21120372 <nguyenhungyen0000@gmail.com>
2025-03-05 20:17:11 +0700

    adding api for update and delete a actor

--- RESTful API/actor/src/main/java/com/hcmus/actor/controller/ActorController.java
+++ RESTful API/actor/src/main/java/com/hcmus/actor/controller/ActorController.java
@@ -1,13 +1,12 @@
 package com.hcmus.actor.controller;
 
 import com.hcmus.actor.dto.ActorDto;
+import com.hcmus.actor.dto.ActorDtoRequest;
 import com.hcmus.actor.dto.ResponseDto;
 import com.hcmus.actor.service.ActorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +20,18 @@ public class ActorController {
     @GetMapping("/list")
     public ResponseEntity<ResponseDto<List<ActorDto>>> getActorsList() {
         ResponseDto<List<ActorDto>> response = actorService.getActorsList();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseDto> deleteAActor(@PathVariable Integer id) {
+        ResponseDto response = actorService.deleteAActor(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ResponseDto> updateAActor(@PathVariable Integer id, @RequestBody ActorDtoRequest actorDtoRequest) {
+        ResponseDto response = actorService.updateAActor(id, actorDtoRequest);
         return ResponseEntity.ok(response);
     }
 }
--- RESTful API/actor/src/main/java/com/hcmus/actor/service/ActorService.java
+++ RESTful API/actor/src/main/java/com/hcmus/actor/service/ActorService.java
@@ -1,6 +1,7 @@
 package com.hcmus.actor.service;
 
 import com.hcmus.actor.dto.ActorDto;
+import com.hcmus.actor.dto.ActorDtoRequest;
 import com.hcmus.actor.dto.ResponseDto;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface ActorService {
 
     ResponseDto<List<ActorDto>> getActorsList();
+    ResponseDto updateAActor(Integer id, ActorDtoRequest actorDtoRequest);
+    ResponseDto deleteAActor(Integer id);
 }
